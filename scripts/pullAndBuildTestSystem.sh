c2abba1bb4da6918764470df44044a06e1469e04
David Karl <david.karl@tec.govt.nz>
2018-06-01 12:51:56 +1200

    Updating script to pull from my repos

--- scripts/pullAndBuildTestSystem.sh
+++ scripts/pullAndBuildTestSystem.sh
@@ -7,12 +7,12 @@ fi
 
 if [ ! -e OneLife ]
 then
-	git clone https://github.com/jasonrohrer/OneLife.git
+	git clone https://github.com/UncleGus/OneLife.git
 fi
 
 if [ ! -e OneLifeData7 ]
 then
-	git clone https://github.com/jasonrohrer/OneLifeData7.git	
+	git clone https://github.com/UncleGus/OneLifeData7.git	
 fi
 
 
