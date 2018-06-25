660828ffb2c5238e8ff6af46c5446b1010634666
David Karl <mr.david.karl@gmail.com>
2018-06-25 16:18:22 +1200

    Commented out clothing spawn stuff

--- server/server.cpp
+++ server/server.cpp
@@ -3274,12 +3274,13 @@ void processLoggedInPlayer( Socket *inSock,
     newObject.yummyBonusStore = 0;
 
     newObject.clothing = getEmptyClothingSet();
-    newObject.clothing.hat = getObject( 199 );
-    newObject.clothing.tunic = getObject( 202 );
-    newObject.clothing.backpack = getObject( 198 );
-    newObject.clothing.hat = getObject( 200 );
-    newObject.clothing.frontShoe = getObject( 203 );
-    newObject.clothing.backShoe = getObject( 203 );
+    // this is to make people spawn with clothes, for testing of weapons and armour
+    // newObject.clothing.hat = getObject( 199 );
+    // newObject.clothing.tunic = getObject( 202 );
+    // newObject.clothing.backpack = getObject( 198 );
+    // newObject.clothing.hat = getObject( 200 );
+    // newObject.clothing.frontShoe = getObject( 203 );
+    // newObject.clothing.backShoe = getObject( 203 );
 
     for( int c=0; c<NUM_CLOTHING_PIECES; c++ ) {
         newObject.clothingEtaDecay[c] = 0;
