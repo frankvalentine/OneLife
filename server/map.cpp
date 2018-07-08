d07831e11823feeaf7fda08f7d5dc47f5f57f6e7
David Karl <david.karl@tec.govt.nz>
2018-07-09 11:28:11 +1200

    Changes to make lifespan two hours instead of one

--- server/map.cpp
+++ server/map.cpp
@@ -135,7 +135,7 @@ static double eveAngle = 2 * M_PI;
 // what human-placed stuff, together, counts as a camp
 static int campRadius = 20;
 
-static float minEveCampRespawnAge = 60.0;
+static float minEveCampRespawnAge = 120.0;
 
 
 extern int apocalypsePossible;
@@ -1938,7 +1938,7 @@ void initMap() {
     edgeObjectID = SettingsManager::getIntSetting( "edgeObject", 0 );
     
     minEveCampRespawnAge = 
-        SettingsManager::getFloatSetting( "minEveCampRespawnAge", 60.0f );
+        SettingsManager::getFloatSetting( "minEveCampRespawnAge", 120.0f );
     
 
     for( int i=0; i<NUM_RECENT_PLACEMENTS; i++ ) {
