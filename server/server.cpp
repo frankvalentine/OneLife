242cd12c1d8e6a3a9ad333418e74a7948cc573e7
David Karl <david.karl@tec.govt.nz>
2018-07-18 14:43:31 +1200

    Added moderator specific login equipment

--- server/server.cpp
+++ server/server.cpp
@@ -3435,16 +3435,16 @@ void processLoggedInPlayer( Socket *inSock,
     newObject.yummyBonusStore = 0;
 
     newObject.clothing = getEmptyClothingSet();
-
-    newObject.clothing = getEmptyClothingSet();
     // this is to make people spawn with clothes, for testing of weapons and armour
-    // newObject.clothing.hat = getObject( 86782 );
-    // newObject.clothing.tunic = getObject( 86778 );
-    // newObject.clothing.backpack = getObject( 86115 );
-    // newObject.clothing.bottom = getObject( 86716 );
-    // newObject.clothing.frontShoe = getObject( 87803 );
-    // newObject.clothing.backShoe = getObject( 87803 );
-    // newObject.holdingID = 86122;
+    if( getNationName( inEmail, &nationMembers ) == "MODERATORS" ) {
+        newObject.clothing.hat = getObject( 86782 );
+        newObject.clothing.tunic = getObject( 86778 );
+        newObject.clothing.backpack = getObject( 198 );
+        newObject.clothing.bottom = getObject( 86716 );
+        newObject.clothing.frontShoe = getObject( 87803 );
+        newObject.clothing.backShoe = getObject( 87803 );
+        newObject.holdingID = 86278;
+    }
 
     for( int c=0; c<NUM_CLOTHING_PIECES; c++ ) {
         newObject.clothingEtaDecay[c] = 0;
