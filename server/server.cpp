7e676a354b98fbb98d7035014c2dbaf73e1616ba
David Karl <david.karl@tec.govt.nz>
2018-07-18 16:31:56 +1200

    Can add my stuff back now

--- server/server.cpp
+++ server/server.cpp
@@ -3358,6 +3358,8 @@ void processLoggedInPlayer( Socket *inSock,
                             char *inEmail,
                             int inTutorialNumber ) {
     
+    printf("Starting to process new login");
+    
     // reload these settings every time someone new connects
     // thus, they can be changed without restarting the server
     minFoodDecrementSeconds = 
@@ -3436,16 +3438,16 @@ void processLoggedInPlayer( Socket *inSock,
 
     newObject.clothing = getEmptyClothingSet();
     // this is to make people spawn with clothes, for testing of weapons and armour
-    // if( getNationName( inEmail, &nationMembers ) != NULL &&
-    //     strcmp( getNationName( inEmail, &nationMembers ), "MODERATORS" ) == 0 ) {
-    //     newObject.clothing.hat = getObject( 86782 );
-    //     newObject.clothing.tunic = getObject( 86778 );
-    //     newObject.clothing.backpack = getObject( 198 );
-    //     newObject.clothing.bottom = getObject( 86716 );
-    //     newObject.clothing.frontShoe = getObject( 87803 );
-    //     newObject.clothing.backShoe = getObject( 87803 );
-    //     newObject.holdingID = 86278;
-    // }
+    if( getNationName( inEmail, &nationMembers ) != NULL &&
+        strcmp( getNationName( inEmail, &nationMembers ), "MODERATORS" ) == 0 ) {
+        newObject.clothing.hat = getObject( 86782 );
+        newObject.clothing.tunic = getObject( 86778 );
+        newObject.clothing.backpack = getObject( 198 );
+        newObject.clothing.bottom = getObject( 86716 );
+        newObject.clothing.frontShoe = getObject( 87803 );
+        newObject.clothing.backShoe = getObject( 87803 );
+        newObject.holdingID = 86278;
+    }
 
     for( int c=0; c<NUM_CLOTHING_PIECES; c++ ) {
         newObject.clothingEtaDecay[c] = 0;
